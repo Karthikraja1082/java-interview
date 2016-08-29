749599214363410bfce63df6130afa7e7df20982
Karthikraja1082 <karthikkind@gmail.com>
2016-08-29 15:17:39 -0700

    Modified the changes as per the requirement

--- src/main/java/edu/ucdavis/afs/dao/EmployeeDaoImpl.java
+++ src/main/java/edu/ucdavis/afs/dao/EmployeeDaoImpl.java
@@ -1,31 +1,68 @@
 package edu.ucdavis.afs.dao;
 
 import edu.ucdavis.afs.model.Employee;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Criteria;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import java.util.List;
 
 @Repository
 public class EmployeeDaoImpl implements EmployeeDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
     @Override
     @Transactional
     public Employee getEmployeeById(Long employeeId) {
-        // TODO: implement this method per requirements
-        return null;
-    }
+		// Implemented this method per requirements
+        String empId = "SELECT e from Employee e WHERE id = :id";
+        TypedQuery<Employee> emp = entityManager.createQuery(empId, Employee.class);
+        q.setParameter("id", employeeId);
+        try {
+            Employee employee = emp.getSingleResult();
+            return employee;
+        } catch (Exception e) {
+            return null;
+        }
+	}
 
     @Override
     @Transactional
     public List<Employee> findEmployees(String firstName, String lastName, String jobTitle) {
-        // TODO: implement this method per requirements
-        return null;
+        // Implemented this method per requirements
+		Criteria criteria = createEntityCriteria();
+		criteria.add(Restrictions.eq("firstName", firstName));
+		criteria.add(Restrictions.eq("lastName", lastName));
+		criteria.add(Restrictions.eq("jobTitle", jobTitle));
+        return (Employee) criteria.uniqueResult();
     }
-
+	
+	
+	protected Criteria createEntityCriteria(){
+        return getSession().createCriteria(persistentClass);
+    }
+	
     @Override
     @Transactional
     public Long saveEmployee(Employee employee) {
-        // TODO: implement this method per requirements
-        return null;
+        // Implemented this method per requirements
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			session.save(employee);
+		} catch (HibernateException he){
+			return he.printStackTrace();
+        }
+		return employee;
     }
 }
